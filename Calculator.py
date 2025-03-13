c415406df587cbaef3c2d8dd6ed44df181274e0c
Vamshi <vamsi@gmail.com>
2025-03-13 20:01:21 +0530

    This is addition feature

--- Calculator.py
+++ Calculator.py
@@ -1,7 +1,8 @@
-a = 10
-b = 20
-def add(a, b):
-    c = a+b
-    return c
-add(a, b)
-print("Addition of two numbers is:", add(a, b))
+a=10
+b=20
+def add(a,b):
+ c=a+b
+ return c
+
+result=add(a,b)
+print(result)
